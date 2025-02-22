f00c972fe0222aa72b4c686b1ca4e721b02bedb7
Pranav Kiran Mahale <pranav@Pranavs-MacBook-Air.local>
2025-02-22 10:49:51 +0530

    Vercel added

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/register" element={<EmployeeRegistration />} />
           <Route path="/employee" element={<EmployeeDetails />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
